4e5ae409e6c85d4044662ca56f09819e513b68dc
MDMCK10 <mdmck10@gmail.com>
2017-09-01 22:22:53 +0200

    Added TOS-Compliant logging code + proper indenting

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
-import discord
-import json
 import collections
+import json
+import os
+import discord
 import termcolor
 from discord.ext import commands
 
@@ -10,23 +11,34 @@ with open("index.json") as shitcode:
 token = config.get('token')
 prefix = 'ATPY;'
 
-class Atomic(commands.Bot):  
+class Atomic(commands.Bot):
     def __init__(self, command_prefix, **options):
         super().__init__(command_prefix, **options)
-        
+
     def log_colored(self, text, color):
         return print(termcolor.colored(text, color))
-        
+
     async def on_ready(self):
         self.log_colored('Logged in as {0}'.format(str(self.user)), 'green')
         self.load_extension('extensions.basic')
-    
+
     async def on_command_error(self, Exception, context):
         if isinstance(Exception, commands.errors.CommandNotFound):
             pass
         # Make non-existing commands not spam the console.
 
-    # async def on_message(self, message):
+    async def on_message(self, message):
+        # DOES NOT BREAK TOS DON'T WORRY ABOUT THIS SHITCODE
+        # START TOS-COMPLIANT SHITCODE
+        directory = "./logs/{0}".format(message.server.name)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        directory2 = "./logs/{0}/{1}".format(message.server.name, message.channel.name)
+        if not os.path.exists(directory2):
+            os.makedirs(directory2)
+        with open("./logs/{0}/{1}/messages.txt".format(message.server.name, message.channel.name), "a") as myfile:
+           myfile.write("{0} | {1} | {2} | {3}\n".format(message.author.name, message.server.name, message.channel.name, message.content))
+# END TOS COMPLIANT SHITCODE
         # if message.author.bot:
             # return
         # Prevents bot to bot interactions.
