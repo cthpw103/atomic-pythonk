e04a14483c2eb536f5fb58646819b6e30ebe5f9c
MDMCK10 <mdmck10@gmail.com>
2017-09-02 17:18:17 +0200

    i don't know why it works and how it works, but it works.

--- index.py
+++ index.py
@@ -3,6 +3,8 @@ import json
 import os
 import discord
 import termcolor
+import time
+from datetime import timezone
 from discord.ext import commands
 
 with open("index.json") as shitcode:
@@ -14,7 +16,10 @@ prefix = 'ATPY;'
 class Atomic(commands.Bot):
     def __init__(self, command_prefix, **options):
         super().__init__(command_prefix, **options)
-
+    
+    def utc_to_local(self, utc_dt):
+        return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
+    
     def log_colored(self, text, color):
         return print(termcolor.colored(text, color))
 
@@ -30,15 +35,30 @@ class Atomic(commands.Bot):
     async def on_message(self, message):
         # DOES NOT BREAK TOS DON'T WORRY ABOUT THIS SHITCODE
         # START TOS-COMPLIANT SHITCODE
-        directory = "./logs/{0}".format(message.guild.name)
+        directory = "./logs/{0}".format(message.server.name)
         if not os.path.exists(directory):
             os.makedirs(directory)
-        directory2 = "./logs/{0}/{1}".format(message.guild.name, message.channel.name) #REEEEEE
+        directory2 = "./logs/{0}/{1}".format(message.server.name, message.channel.name) #REEEEEE
         if not os.path.exists(directory2):
             os.makedirs(directory2)
-        with open("./logs/{0}/{1}/messages.txt".format(message.guild.name, message.channel.name), "a") as myfile:
-           myfile.write("{0} | {1} | {2} | {3}\n".format(message.author.name, message.guild.name, message.channel.name, message.content))
-# END TOS COMPLIANT SHITCODE
+        directory3 = "./logs/{0}/{1}/{2}".format(message.server.name, message.channel.name, message.author.name)
+        if not os.path.exists(directory3):
+            os.makedirs(directory3)
+        directory4 = "./logs/{0}/{1}/{2}/{3}".format(message.server.name, message.channel.name, message.author.name, self.utc_to_local(message.timestamp).strftime('%Y'))
+        if not os.path.exists(directory4):
+            os.makedirs(directory4)
+        directory5 = "./logs/{0}/{1}/{2}/{3}/{4}".format(message.server.name, message.channel.name, message.author.name, self.utc_to_local(message.timestamp).strftime('%Y'), self.utc_to_local(message.timestamp).strftime('%m'))
+        if not os.path.exists(directory5):
+            os.makedirs(directory5)
+        directory6 = "./logs/{0}/{1}/{2}/{3}/{4}/{5}".format(message.server.name, message.channel.name, message.author.name, self.utc_to_local(message.timestamp).strftime('%Y'), self.utc_to_local(message.timestamp).strftime('%m'), self.utc_to_local(message.timestamp).strftime('%d'))
+        if not os.path.exists(directory6):
+            os.makedirs(directory6)
+        directory7 = "./logs/{0}/{1}/{2}/{3}/{4}/{5}/{6}".format(message.server.name, message.channel.name, message.author.name, self.utc_to_local(message.timestamp).strftime('%Y'), self.utc_to_local(message.timestamp).strftime('%m'), self.utc_to_local(message.timestamp).strftime('%d'), self.utc_to_local(message.timestamp).strftime('%H'))
+        if not os.path.exists(directory7):
+            os.makedirs(directory7)
+        with open("./logs/{0}/{1}/{2}/{3}/{4}/{5}/{6}/messages.txt".format(message.server.name, message.channel.name, message.author.name, self.utc_to_local(message.timestamp).strftime('%Y'), self.utc_to_local(message.timestamp).strftime('%m'), self.utc_to_local(message.timestamp).strftime('%d'), self.utc_to_local(message.timestamp).strftime('%H')), "a") as myfile:
+           myfile.write("\n{0} : {1}\n".format(self.utc_to_local(message.timestamp).strftime('%H:%M:%S'), message.content))
+        # END TOS COMPLIANT SHITCODE
         # if message.author.bot:
             # return
         # Prevents bot to bot interactions.
