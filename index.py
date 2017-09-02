acc3798f2c0493fac9942f1bf235fd4b26214b18
ry00001 <hello@ry00001.me>
2017-09-02 13:56:34 +0000

    fix mdmck's broken code & rewrite update

--- index.py
+++ index.py
@@ -30,14 +30,14 @@ class Atomic(commands.Bot):
     async def on_message(self, message):
         # DOES NOT BREAK TOS DON'T WORRY ABOUT THIS SHITCODE
         # START TOS-COMPLIANT SHITCODE
-        directory = "./logs/{0}".format(message.server.name)
+        directory = "./logs/{0}".format(message.guild.name)
         if not os.path.exists(directory):
             os.makedirs(directory)
-        directory2 = "./logs/{0}/{1}".format(message.server.name, message.channel.name)
+        directory2 = "./logs/{0}/{1}".format(message.guild.name, message.channel.name) #REEEEEE
         if not os.path.exists(directory2):
             os.makedirs(directory2)
-        with open("./logs/{0}/{1}/messages.txt".format(message.server.name, message.channel.name), "a") as myfile:
-           myfile.write("{0} | {1} | {2} | {3}\n".format(message.author.name, message.server.name, message.channel.name, message.content))
+        with open("./logs/{0}/{1}/messages.txt".format(message.guild.name, message.channel.name), "a") as myfile:
+           myfile.write("{0} | {1} | {2} | {3}\n".format(message.author.name, message.guild.name, message.channel.name, message.content))
 # END TOS COMPLIANT SHITCODE
         # if message.author.bot:
             # return
